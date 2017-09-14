2364130e1e9969f44c998dd234027ebea2d03e34
Jyoti Rani <ranijyoti392@gmail.com>
2017-09-14 22:59:14 +0530

    Initial commit

---
+++ src/Math.kt
@@ -0,0 +1,24 @@
+/**
+ * Created by jyotirani on 14/09/17.
+ */
+fun main(args: Array<String>) {
+
+    //addition
+
+    var x = 10
+    var y = 20
+
+    val sum = x + y
+
+    val result = " $x + $y = $sum"
+
+    println(result)
+
+    //subtraction logic
+    val a = 5
+    val b = 2
+
+    println(" $a - $b = ${a-b}")
+
+    
+}
---
+++ src/app.kt
@@ -0,0 +1,12 @@
+/**
+ * Created by jyotirani on 14/09/17.
+ */
+
+fun main(args: Array<String>) {
+
+    println("Name   :   Jyoti Rani")
+    println("Place  :   Pune")
+    println("Age    :   25")
+    println("Gender :   Female")
+
+}
